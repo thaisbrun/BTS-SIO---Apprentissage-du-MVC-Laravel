3db977be142d999415e779a6c1a4975f4a257f78
thaisbrun <90321708+thaisbrun@users.noreply.github.com>
2021-10-07 15:57:17 +0200

    Add files via upload

---
+++ thaistravailslam4/mini-mvc-sample-1.4/views/global/about.php
@@ -0,0 +1,12 @@
+<div class="container mt-5">
+    <div class="row">
+        <div class="card">
+            <div class="card-body">
+                <h3>About</h3>
+                <p>
+                    A propos de 
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
