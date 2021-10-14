97e511c24cc727b8eab0062d0401661c2d6b3fbb
thaisbrun <90321708+thaisbrun@users.noreply.github.com>
2021-10-14 15:38:13 +0200

    V1

---
+++ mini-mvc-sample-1.4/models/TodoModel.php
@@ -0,0 +1,31 @@
+<?php
+namespace models;
+
+use models\base\SQL;
+
+class TodoModel extends SQL
+{
+    public function __construct()
+    {
+        parent::__construct('todos', 'id');
+    }
+    function todoNonTermine()
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM todos WHERE termine = 0;");
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+    function marquerCommeTermine($id){
+        $stmt = $this->pdo->prepare("UPDATE todos SET termine = 1 WHERE id = ?");
+        $stmt->execute([$id]);
+    }
+    function ajouterTodo($texte){
+        $stmt = $this->pdo->prepare("INSERT INTO todos VALUES (null, ?, 0, null);");
+        $stmt->execute([$texte]);
+    }
+    function marquerCommeSupprime($id){
+        $stmt = $this->pdo->prepare("DELETE FROM todos WHERE id = ? AND termine = 1");
+        $stmt->execute([$id]);
+    }
+
+}
